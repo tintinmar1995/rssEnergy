da391571539c70e3bcb61f05eb19788ccd37bd2e
Martin MASSON <martin.masson@protonmail.com>
2025-06-02 08:13:20 +0200

    CLEAN: Update parsers.py

--- rssEnergy/parsers.py
+++ rssEnergy/parsers.py
@@ -1,119 +1,66 @@
 from selenium.webdriver.common.by import By
-
 import locale
 import calendar
 import datetime
 import hashlib
 
 def new_article(**kw):
-    out =  {k: kw.get(k, None) for k in [
+    out = {k: kw.get(k, None) for k in [
         'category', 'image', 'link', 'title', 'author', 'description',
         'language', 'copyright', 'pubDate'
     ]}
-
     out['guid'] = hashlib.sha256(out['link'].encode()).hexdigest()
     return out
- 
-def iea_news(driver):
 
-    driver.get("https://www.iea.org/news")
+def parse_date(date_str, lang):
+    locale.setlocale(locale.LC_ALL, lang)
+    months = {calendar.month_name[month_idx].lower(): month_idx for month_idx in range(1, 13)}
+    dt_parts = date_str.split(' ')
+    dt_parts[0] = int(dt_parts[0])
+    dt_parts[1] = months[dt_parts[1]]
+    dt_parts[2] = int(dt_parts[2])
+    dt_parts.reverse()
+    return datetime.datetime(*dt_parts).strftime("%Y-%m-%d %H:%M:%S")
 
-    articlesContainer = driver.find_element(By.CLASS_NAME, 'o-layout__main')
-    articles = articlesContainer.find_elements(By.TAG_NAME, "article")
+def scrape_articles(driver, url, container_class, article_class, lang, title_class, date_class, img_class, link_class, desc_class=None):
+    driver.get(url)
+    articlesContainer = driver.find_element(By.CLASS_NAME, container_class)
+    articles = articlesContainer.find_elements(By.CLASS_NAME, article_class)
 
-    parsedArticles = list()
+    parsedArticles = []
     for article in articles:
+        img = article.find_element(By.TAG_NAME, img_class).get_property("src")
+        title = article.find_element(By.CLASS_NAME, title_class).text
+        dt = article.find_element(By.CLASS_NAME, date_class).text
+        link = article.find_element(By.TAG_NAME, link_class).get_property('href')
         
-        tag = article.find_element(By.CLASS_NAME, "a-tag-small").text
-        try:
-            img = article.find_element(By.TAG_NAME, "img").get_property("src")
-        except:
-            img = None
-        title = article.find_element(By.CLASS_NAME, "m-news-detailed-listing__title").text
-        dt = article.find_element(By.CLASS_NAME, "m-news-detailed-listing__date").text
+        pubDate = parse_date(dt, lang)
         
-        link = article.find_element(By.CLASS_NAME, "m-news-detailed-listing__link").get_property('href')
-
-        locale.setlocale(locale.LC_ALL, 'en_UK')
-        dt = datetime.datetime.strptime(dt, "%d %B %Y").strftime("%Y-%m-%d %H:%M:%S")
-
-        parsedArticles.append(new_article(
-            category=tag, image=img, pubDate=dt, title=title, link=link
-        ))
+        article_data = {
+            'image': img,
+            'pubDate': pubDate,
+            'title': title,
+            'link': link
+        }
+        
+        if desc_class:
+            desc = article.find_element(By.CLASS_NAME, desc_class).text
+            article_data['description'] = desc
+        
+        parsedArticles.append(new_article(**article_data))
 
     driver.quit()
     return parsedArticles
 
-
+def iea_news(driver):
+    return scrape_articles(driver, "https://www.iea.org/news", 'o-layout__main', 'article', 'en_UK',
+                           "m-news-detailed-listing__title", "m-news-detailed-listing__date", "img", "m-news-detailed-listing__link")
 
 def rte_actualites(driver):
+    return scrape_articles(driver, "https://www.rte-france.com/actualites", "result-container", "a", 'fr_FR',
+                           "read-more-title", "file-date", "img")
 
-    locale.setlocale(locale.LC_ALL, 'fr_FR')
-    months = {
-        calendar.month_name[month_idx].upper():
-        month_idx for month_idx in range(1,13)
-    }
-
-    driver.get("https://www.rte-france.com/actualites")
-
-    articlesContainer = driver.find_element(By.CLASS_NAME, "result-container")
-    articles = articlesContainer.find_elements(By.TAG_NAME, "a")
-
-    parsedArticles = list()
-    for article in articles:
-        img = article.find_element(By.TAG_NAME, "img")
-        title = article.find_element(By.CLASS_NAME, "read-more-title")
-
-        dt = article.find_element(By.CLASS_NAME, "file-date").text.split(' ')
-        dt[0] = int(dt[0])
-        dt[1] = months[dt[1]]
-        dt[2] = int(dt[2])
-        dt.reverse()
-        dt = datetime.datetime(*dt).strftime("%Y-%m-%d %H:%M:%S")
-
-        parsedArticles.append(new_article(
-            image=img.get_property("src"), pubDate=dt,
-            title=title.text, link=article.get_property('href')
-        ))
-
-    driver.quit()
-    return parsedArticles
-
-
-def enedis_odte(driver, lang = 'fr_FR'):
-
-    locale.setlocale(locale.LC_ALL, lang)
-    months = {
-        calendar.month_name[month_idx].lower():
-        month_idx for month_idx in range(1,13)
-    }
-
-    driver.get("https://observatoire.enedis.fr/tous-les-articles")
-
-    articlesContainer = driver.find_element(By.CLASS_NAME, "views-infinite-scroll-content-wrapper")
-    articles = articlesContainer.find_elements(By.CLASS_NAME, "views-row")
-
-    parsedArticles = list()
-    for article in articles:
-
-        img = article.find_element(By.TAG_NAME, "img")
-        link = article.find_element(By.TAG_NAME, "a")
-        title = article.find_element(By.CLASS_NAME, "article-card__body__title").text
-
-        dt = article.find_element(By.CLASS_NAME, "article-card__body__date").text.split(' ')
-        dt[0] = int(dt[0])
-        dt[1] = months[dt[1]]
-        dt[2] = int(dt[2])
-        dt.reverse()
-        dt = datetime.datetime(*dt).strftime("%Y-%m-%d %H:%M:%S")
-
-        desc = article.find_element(By.CLASS_NAME, "article-card__body__txt").text
-
-        parsedArticles.append(new_article(
-            image=img.get_property("src"), pubDate=dt,
-            title=title, link=link.get_property('href'),
-            description=desc, language=lang
-        ))
-
-    driver.quit()
-    return parsedArticles
+def enedis_odte(driver, lang='fr_FR'):
+    return scrape_articles(driver, "https://observatoire.enedis.fr/tous-les-articles", 
+                           "views-infinite-scroll-content-wrapper", "views-row", lang,
+                           "article-card__body__title", "article-card__body__date", "img", "a", "article-card__body__txt")
